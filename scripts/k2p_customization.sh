243d9cf6f8293b2626a1b2b80651be34db437301
smsmail2022 <123883726+smsmail2022@users.noreply.github.com>
2023-01-31 21:13:38 +0800

    Update k2p_customization.sh

--- scripts/k2p_customization.sh
+++ scripts/k2p_customization.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+
 set -ex
 
 # Allow chips that support 11ac to use 256QAM on 2.4GHz
 cp -rf ../immortalwrt/package/kernel/mt76/patches package/kernel/mt76/
 
-
-
-source ./01_customize_packages.sh
-
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
 # 修改默认ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+source ./01_customize_packages.sh
 
 exit 0
